ebfa0992068df7eb6fd2f5862de7cdb5e141755b
Ermakov Alexandr <ermakov.sascha2015yandex.ru@gmail.com>
2023-09-29 16:07:23 +0300

    feat: add Neighbourhood params

---
+++ src/main/java/by/lease/entity/enums/Neighbourhood.java
@@ -0,0 +1,18 @@
+package by.lease.entity.enums;
+
+public enum Neighbourhood {
+    HIGH_SCHOOL,
+    ELEMENTARY_SCHOOL,
+    DAYCARE,
+    PEDESTRIAN_FRIENDLY,
+    CYCLING_FRIENDLY,
+    TRANSIT_FRIENDLY,
+    CAR_FRIENDLY,
+    GROCERY_STORES,
+    COFFEE_SHOP,
+    SHOPPING,
+    RESTAURANT,
+    PARK,
+    QUIET,
+    NIGHTLIFE,;
+}
